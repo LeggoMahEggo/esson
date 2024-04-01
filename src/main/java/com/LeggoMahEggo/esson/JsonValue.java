46f247ed532806500d490ca482b9114a8ccf676d
Yehuda Broderick <46563066+LeggoMahEggo@users.noreply.github.com>
2024-04-01 09:15:02 +0300

    Add LGPL 3.0 license to esson

--- src/main/java/com/LeggoMahEggo/esson/JsonValue.java
+++ src/main/java/com/LeggoMahEggo/esson/JsonValue.java
@@ -1,5 +1,19 @@
-package com.LeggoMahEggo.esson;
+/*
+ Copyright 2024 Yehuda Broderick
+ */
+/*
+ This file is part of esson.
 
+ esson is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+ esson is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License along with esson. If not, see
+  <https://www.gnu.org/licenses/>.
+ */
+package com.LeggoMahEggo.esson;
 
 /**
  * Represents some legal JSON value, wrapping a Java value. Valid types are null, Boolean, String, Long, Double, Integer
